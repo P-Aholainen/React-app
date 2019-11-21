0a919499e9e20dff794bce60b1da5ebe4a96d6e3
P-Aholainen <mpohjoisaho@gmail.com>
2019-11-21 20:27:26 +0200

    päivitys

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return(
        <nav className="navbar">
-        <span className="navbar">LyricsFinder</span>
+        <span className="navbar">EaaS (Earworm as a Service)</span>
        
        </nav>
     );
